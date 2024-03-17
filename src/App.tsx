abd10f681ffa2b317417c64a6aa21bb267969159
JosepConejero <bermeu@gmail.com>
2024-03-17 13:22:52 +0100

    Removed unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
+/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */
+
+//https://create-react-app.dev/docs/adding-typescript/
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>LINKS</h1>
       </header>
-    </div>
+    </>
   );
 }
 
