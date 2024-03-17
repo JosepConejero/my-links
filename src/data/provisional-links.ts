3fd9b72f8adfbecf260f9a1baf15f3358a3eae65
JosepConejero <bermeu@gmail.com>
2024-03-17 13:59:26 +0100

    Added folder data and file provisional-links-ts

---
+++ src/data/provisional-links.ts
@@ -0,0 +1,6 @@
+export const provisionalLinks = [
+  { address: "https://reactjs.org", linkName: "REACT" },
+  { address: "https://create-react-app.dev/docs/adding-typescript/", linkName: "CREATE-REACT-APP WITH TS" },
+];
+
+export default provisionalLinks;
