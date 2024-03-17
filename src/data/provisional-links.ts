fc7ca28e646a6966d4ee895df9f1569df2463947
JosepConejero <bermeu@gmail.com>
2024-03-17 14:02:13 +0100

    Modified linkNames from provisional-links.ts

--- src/data/provisional-links.ts
+++ src/data/provisional-links.ts
@@ -1,6 +1,6 @@
 export const provisionalLinks = [
-  { address: "https://reactjs.org", linkName: "REACT" },
-  { address: "https://create-react-app.dev/docs/adding-typescript/", linkName: "CREATE-REACT-APP WITH TS" },
+  { address: "https://reactjs.org", linkName: "React" },
+  { address: "https://create-react-app.dev/docs/adding-typescript/", linkName: "create-react-app with TS" },
 ];
 
 export default provisionalLinks;
