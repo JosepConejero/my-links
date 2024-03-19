5bc19fd81e393fe8f039bba4619656092666bde1
JosepConejero <bermeu@gmail.com>
2024-03-19 20:30:50 +0100

    Added some links to provisional-links.ts

--- src/data/provisional-links.ts
+++ src/data/provisional-links.ts
@@ -5,7 +5,8 @@ export const provisionalLinks: MyLink[] = [
   { address: "https://google.es", linkName: "Google", order: 1, folder: "ROOT" },
   { address: "https://guardias-invited.netlify.app/guardias", linkName: "Guardias invited", order: 1, folder: "ROOT" },
   { address: "https://learning-helper-app.netlify.app/home", linkName: "Learning helper", order: 1, folder: "ROOT" },
-  { address: "https://reactjs.org", linkName: "React", order: 1, folder: "ROOT" },
+  { address: "https://react.dev/learn", linkName: "React overview", order: 1, folder: "ROOT" },
+  { address: "https://react.dev/reference/react/hooks", linkName: "React hooks", order: 1, folder: "ROOT" },
   //{ address: "https://create-react-app.dev/docs/adding-typescript/", linkName: "create-react-app with TS", order: 1, folder: "ROOT" },
   { address: "https://github.com/sudheerj/reactjs-interview-questions", linkName: "PREGUNTAS ENTREVISTA", order: 1, folder: "ROOT" },
   { address: "https://developer.mozilla.org/en-US/docs/Web/JavaScript", linkName: "Javascript MDN", order: 1, folder: "ROOT" },
@@ -14,6 +15,8 @@ export const provisionalLinks: MyLink[] = [
   { address: "https://developer.mozilla.org/en-US/docs/Learn/CSS", linkName: "CSS MDN", order: 1, folder: "ROOT" },
   { address: "https://lenguajecss.com/css/#introduccion", linkName: "CSS", order: 1, folder: "ROOT" },
   { address: "https://www.notion.so/PROGRAMMING-4d871bcb4af746019192b643380a55de", linkName: "Notion", order: 1, folder: "ROOT" },
+  { address: "https://react.dev/learn/typescript", linkName: "TS React", order: 1, folder: "ROOT" },
+  { address: "https://www.typescriptlang.org/docs/handbook/2/everyday-types.html", linkName: "TS", order: 1, folder: "ROOT" },
 ];
 
 export default provisionalLinks;
