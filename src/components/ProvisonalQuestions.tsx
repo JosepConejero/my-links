6c75640ad0527651818fbcea3b7296157e8135c1
JosepConejero <bermeu@gmail.com>
2024-03-21 17:56:23 +0100

    Added more provisional questions

---
+++ src/components/ProvisonalQuestions.tsx
@@ -0,0 +1,69 @@
+export const ProvisonalQuestions = () => {
+  return (
+    <div>
+      <ul className="ul-class">
+        <h3>PROYECTOS PERSONALES</h3>
+        <li>Me hago mis propias herramientas (android, etc.)</li>
+
+        <h3>MOTIVACIONES</h3>
+        <li>Aprender cosas nuevas</li>
+        <li>Reto de resolver problemas</li>
+
+        <h3>HABILIDADES</h3>
+        <li>Metódico pero adaptable</li>
+        <li>Flexibilidad</li>
+        <li>Intento hacer las cosas bien</li>
+        <li>Respetuoso con los compañeros, dialogador, pacificador, agoto la vía diplomática</li>
+        <li>Habilidad para obedecer y mandar</li>
+        <li>Gestión de equipos</li>
+        <li>Constante</li>
+        <li>Responsable</li>
+        <li>Organizado</li>
+        <li>Reconocer errores</li>
+        <li>Creativo</li>
+
+        <h3>ÁREAS DE MEJORA</h3>
+        <li>Dificultad para concentrase, pero con un ambiente en casa silencioso...</li>
+        <li>Un poco introvertido; cuando llevo un tiempo o la gente es abierta...</li>
+        <li>Dificultad para romper el hielo cuando estoy con gente nueva</li>
+        <li>Cuando hay muchas opciones me cuesta más que cuando tengo un camino claro y marcado; debo encontrar mejores formas de converir la maraña de opciones en caminos claros y marcados</li>
+        <li>Puntilloso cuando las cosas se hacen de cualquier manera, pero cada vez me mentalizo más deprisa</li>
+
+        <h3>CON QUIÉN NO TRABAJARÍA</h3>
+        <li>Personas que maltraten / inflexibles o cabezones que se obstinan en el error</li>
+
+        <h3>COSAS DE LAS QUE SENTIRSE ORGULLOSO</h3>
+        <li>Cuando tengo un objetivo claro, lucho hasta conseguirlo</li>
+
+        <h3>ALGO DE LO QUE NO ESTÉ ORGULLOSO</h3>
+        <li>No haber empezado a prepararme antes para ser programador</li>
+
+        <h3>ALQUIEN A QUIEN ADMIRE</h3>
+        <li>Mis padres /dan sin esperar nada a cambio, generosidad absoluta (altruismo, gratuidad)</li>
+
+        <h3>EMPRESAS PREFERIDAS</h3>
+        <li>Serias / teletrabajo</li>
+
+        <h3>PREGUNTAS</h3>
+        <li>Nombre de la empresa</li>
+        <li>Horario</li>
+        <li>Sueldo, si es revisable</li>
+        <li>Teletrabajo</li>
+        <li>Stack (herramientas que usan)</li>
+        <li>Si es start-up o consultora</li>
+        <li>Equipos de muchas / pocas personas</li>
+        <li>Si se cuenta con apoyo de compañeros</li>
+        <li>Si hay on boarding</li>
+        <li>Si hay posibilidad de promoción</li>
+        <li>Si son proyectos de larga o corta duración</li>
+        <li>Tipo de tareas que realizaría</li>
+
+        <h3>HOBBIES</h3>
+        <li>Música, cine, literatura, nutrición, naturaliza, dibujo, programación, videojuegos, teatro</li>
+
+        <h3>FEEDBACK</h3>
+        <li>Qué le he parecido, si encajo; y si no, por qué no</li>
+      </ul>
+    </div>
+  );
+};
